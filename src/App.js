d39bd9eb7e424eeab009168f2fc30507807ebb4f
Tremain-Ajena Jones <tremainajena.j@gmail.com>
2020-02-24 16:55:27 -0600

    complete walkthrough

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'isomorphic-fetch';
+import 'es6-promise';
+// YOU HAVE TO INSTALL THE 'ISOMORPHIC-FETCH' AND 'ES6-PROMISE'
+// MANUALLY EACH TIME THROUGH THE TERMINAL BY TYPING IN "npm install es6-promise" and "npm install isomorphic-fetch"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    fetch("https://ghibliapi.herokuapp.com/films")
+    .then(res => res.json())
+    // .then(res => console.log(res));
+    // .then(res => res.json())
+    .then(obj => console.log(obj));
+    // 'res' stands for response in this situation
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className= 'App-title'> Welcome to React</h1>
+        </header>
+          <p className= 'App-intro'>
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+      </div>
+    );
+
+  }
 }
 
 export default App;
